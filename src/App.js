db2ce22aeb8989a53e523fd667626cf2f675891e
shivani soni <kajalsoni1845@gmail.com>
2023-12-13 17:47:12 +0530

    FirstCommit 13Dec

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// import './css/bootstrap.css'
+// import './css/style.css'
+// import './css/classic.css'
+// import './css/flexslider.css'
+// import './css/font-awesome.css'
+// import './css/jquery.bxslider.css'
+// import './css/jquery.countdown.css'
+// import './css/jquery.fancybox-1.3.4.css'
+// import './css/jquery.fancybox.css'
+// import './css/magnific-popup.css'
+// import './css/responsive.css'
+// import './css/settings.css'
+// import './css/fonts/fontawesome-webfontba72.svg'
+// import './css/fonts/fontawesome-webfontba72.eot'
+// import './css/fonts/fontawesome-webfontba72.svg'
+// import './css/fonts/fontawesome-webfontba72.ttf'
+// import './css/fonts/fontawesome-webfontba72.woff'
+// import './css/fonts/fontawesome-webfontd41d.eot'
 
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import Header from './components/layout/Header'
+// import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+
+
+// import { useEffect, useState } from 'react'
+// import { auth } from './firebase'
+
+function App () {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        {/* <Header /> */}
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          {/* <Route exact path='/' element={<H/>} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
